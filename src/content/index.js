71729875ef74d671ac602ca1731a257a4b2ac120
Flavien Lebrun <prolebrunflavien@gmail.com>
2026-07-13 17:17:34 +0200

    feat: switching blacklist from hardcoded value to navigator local storing

--- src/content/index.js
+++ src/content/index.js
@@ -4,5 +4,12 @@ console.log('[Mashinted] Content script loaded.');
 
 // Selector for the hydrated feed container on both supported layouts.
 import { startObserver } from './observers.js';
+import { ensureBrandBlacklistStorageReady } from './storage.js';
 
-startObserver();
+ensureBrandBlacklistStorageReady()
+    .catch((error) => {
+        console.error('[Mashinted] Failed to initialize blacklist storage.', error);
+    })
+    .finally(() => {
+        startObserver();
+    });
